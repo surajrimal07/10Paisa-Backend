f4ad133861429fdc67cd13388db2d555bedb6800
Suraj Rimal <davidparkedme@gmail.com>
2024-06-22 01:24:54 +0545

    dd

--- server/redisServer.js
+++ server/redisServer.js
@@ -3,22 +3,27 @@ import { createClient } from 'redis';
 import { hostName } from '../database/dbConfig.js';
 import { mainLogger } from '../utils/logger/logger.js';
 
-export const redisclient = createClient({
-    password: hostName == 'instance-20240618-2207' ? process.env.REDIS_HOST_PROD_LOCAL : process.env.REDIS_PASSWORD_PROD,
-    socket: {
-        host: hostName == 'instance-20240618-2207' ? process.env.REDIS_HOST_PROD_LOCAL : process.env.REDIS_HOST_PROD,
-        port: hostName == 'instance-20240618-2207' ? process.env.REDIS_PORT_PROD_LOCAL : process.env.REDIS_PORT_PROD
-    },
-    connectTimeout: hostName == 'instance-20240618-2207' ? process.env.REDIS_PROD_TIMEOUT_LOCAL : process.env.REDIS_TIMEOUT_PROD,
+export let redisclient;
 
-    retry_strategy: (options) => {
-        mainLogger.info(`Redis Reconnect Attempt: ${options.attempt}`);
-        if (options.attempt <= 5) {
-            return Math.min(options.attempt * 100, 3000);
+if (hostName == 'instance-20240618-2207') {
+    redisclient = createClient();
+} else {
+    redisclient = createClient({
+        password: process.env.REDIS_PASSWORD_PROD,
+        socket: {
+            host: process.env.REDIS_HOST_PROD,
+            port: process.env.REDIS_PORT_PROD
+        },
+        connect_timeout: parseInt(process.env.REDIS_TIMEOUT_PROD),
+        retry_strategy: (options) => {
+            console.log(`Redis Reconnect Attempt: ${options.attempt}`);
+            if (options.attempt <= 5) {
+                return Math.min(options.attempt * 100, 3000);
+            }
+            return 5000;
         }
-        return 5000;
-    }
-});
+    });
+}
 
 export async function saveToRedis(key, value) {
     redisclient.set(key, JSON.stringify(value));
